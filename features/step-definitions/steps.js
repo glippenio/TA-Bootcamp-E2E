afd1102db9f52c5561621a5e8a4209a651f0da77
Anton Filipenka <afilipenka@Antons-MacBook-Pro.local>
2022-12-01 11:46:10 +0300

    newupdates

--- features/step-definitions/steps.js
+++ features/step-definitions/steps.js
@@ -1,13 +1,14 @@
 const { Given, When, Then } = require('@wdio/cucumber-framework');
+const homePage = 'https://www.newegg.com/';
 
 Given("I am on the home page", async () => {
-    await browser.url(`https://www.newegg.com`);
+    await browser.url(homePage);
     await browser.pause(5000)
 });
 
 Given("Close the promo banner if it appears", async () => {
-    const bunnerButton=await $(`#modal-Website > div.modal-dialog.modal-dialog-centered > div > button`); 
-    const logo = await $(` #app > header > div.page-content-inner > div:nth-child(1) > div.section-left.auto-flex > div.header2021-logo > a > img`)
+    const bunnerButton=await $(`.close`); 
+    const logo = await $(`.header2021-logo`)
     try {
         await logo.click()
         } catch {
@@ -15,32 +16,32 @@ Given("Close the promo banner if it appears", async () => {
 });
 
 When("Entry the word Windows in the search bar", async () => {
-    const searchButton = await $(`//*[@id="app"]/header/div[1]/div[1]/div[1]/div[4]/form/div/div[3]/button`)
-    const searchField =  await $('//*[@id="app"]/header/div[1]/div[1]/div[1]/div[5]/form/div/div[1]/input')
+    const searchButton = await $(`.header2021-search-button`)
+    const searchField =  await $('input[type="search"]')
     await searchButton.click()
     await searchField.setValue('windows');
 });
 
 When("Click the search", async () => {
-    const searchButton =  await $(`//*[@id="app"]/header/div[1]/div[1]/div[1]/div[5]/form/div/div[2]/button`)
+    const searchButton =  await $(`.ico.ico-search`)
     await searchButton.click()
 });
 
 Then("Check that at least one item appears", async () => {
-    const elem = await $(`//*[@id="app"]/div[3]/section/div/div/div[2]/div/div/div/div[2]/div/div/div[2]`)
+    const elem = await $(`.item-cell`)
     await expect(elem).toBeExisting();
 });
 
 When("Open Today's Best Deals tab", async () => {
-    const tabButton =   await $(`//*[@id="trendingBanner_720202"]/span`)
+    const tabButton =   await $(`#trendingBanner_720202`)
     await tabButton.click()
 });
 
 When("Click on the Internet shop logo", async () => {
-    const logo = await $(` #app > header > div.page-content-inner > div:nth-child(1) > div.section-left.auto-flex > div.header2021-logo > a > img`)
+    const logo = await $(`.header2021-logo`)
     await logo.click()
 });
 
 Then("Check that the main page opened", async () => {
-    await expect(browser).toHaveUrl('https://www.newegg.com/')
+    await expect(browser).toHaveUrl(homePage)
 });
